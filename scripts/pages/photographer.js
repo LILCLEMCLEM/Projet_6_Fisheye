d4b2cc348a7ba7cf4d1814775e0db50fec3a0e9a
LILCLEMCLEM <clement59lesne@gmail.com>
2023-06-25 12:08:15 +0200

    update

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -15,6 +15,22 @@ async function init_page(){
     const media = photographer_medias(photographers); 
     media.display_header(photographers);
     media.display_data();
+
+    //affichage du message
+    document.getElementById("contact_button").addEventListener("click", function(event){
+        event.preventDefault()
+        const prenom = document.getElementById("prenom");
+        console.log(prenom.value)
+
+        const nom = document.getElementById("nom");
+        console.log(nom.value)
+
+        const email = document.getElementById("email");
+        console.log(email.value)
+
+        const message = document.getElementById("message");
+        console.log(message.value)
+      });
 }
 
 init_page();
